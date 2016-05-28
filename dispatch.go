14aa40abb623434629bf61ae79f577c358d25161
Matthew Walls <matthewjwalls@googlemail.com>
2016-05-28 04:55:42 +0100

    Documentation

--- dispatch.go
+++ dispatch.go
@@ -5,6 +5,14 @@ import (
 	"github.com/MatthewJWalls/configanywhere/providers"	
 )
 
+// Dispatch and the convenience methods in this file
+// allow us to support an API in the short format:
+//
+//     configanywhere.Json(xs).FromFile(ys)
+//
+// which due to Go's lack of support for Traits means
+// we have to do this somewhat clumsily as below.
+
 type Format interface {
 	Using([]byte)
 }
@@ -12,6 +20,8 @@ type Format interface {
 type Dispatch struct {
 	F Format
 }
+
+// Convenience methods for providers
 
 func (this Dispatch) FromFile(filename string) {
 	this.F.Using(providers.NewFileProvider(filename).GetBytes())
@@ -28,6 +38,8 @@ func (this Dispatch) FromString(text string) {
 func (this Dispatch) FromEnvironment() {
 	this.F.Using(providers.NewEnvironmentProvider().GetBytes())
 }
+
+// convenience methods for formats
 
 func Json(target interface{}) Dispatch {
 	return Dispatch{ formats.NewJsonFormat(target) }
